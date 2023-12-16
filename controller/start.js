4a430a88d099db3dd8eb9f89ddde535c6b5eeba4
killica <lazarsavic2003@gmail.com>
2023-12-16 01:03:20 +0100

    Lazar dodao zutu pozadinu

--- controller/start.js
+++ controller/start.js
@@ -1,16 +1,21 @@
 const taxiModel = require('../model/taxi');
 
+function genQRCode(phoneNumber, taxiPermit)
+{
+    return "7824 7236 " + phoneNumber.substr(phoneNumber.length - 3, 3) + taxiPermit.charAt(0) + " " + taxiPermit.substr(1);
+}
+
 module.exports.start = async function(req, res, next){
     try{
         const data = req.body;
         
         if(data.ind === 'signup'){
-            console.log('pozdrav')
             await taxiModel.insertTaxiDriver(data.telefon, data.dozvola, data.ime, data.prezime, data.datum_rodjenja);
         }
         let user = await taxiModel.findTaxiDriver(data.dozvola);
-        console.log('dohvatio sam ', user);
-        res.render('start.ejs', user);
+        let qr = genQRCode(user.broj_telefona, user.taksi_dozvola);
+        console.log(qr);
+        res.render('start.ejs', {user, qr});
     }catch(err){
         next(err);
     }
