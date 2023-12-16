7808f07b67d3e7d678f77d15f8fde2bf34c829dc
stasadjordjevic <stasadjordjevic2@gmail.com>
2023-12-16 08:44:01 +0100

    artikli.ejs i start.js kontroler, anjin commit

--- controller/start.js
+++ controller/start.js
@@ -2,6 +2,8 @@ const taxiModel = require('../model/taxi');
 const fuelModel = require('../model/fuel');
 const artikalModel = require('../model/artikal');
 
+let user;
+
 function genQRCode(phoneNumber, taxiPermit)
 {
     return "7824 7236 " + phoneNumber.substr(phoneNumber.length - 3, 3) + taxiPermit.charAt(0) + " " + taxiPermit.substr(1);
@@ -14,7 +16,12 @@ module.exports.start = async function(req, res, next){
         if(data.ind === 'signup'){
             await taxiModel.insertTaxiDriver(data.telefon, data.dozvola, data.ime, data.prezime, data.datum_rodjenja);
         }
-        let user = await taxiModel.findTaxiDriver(data.dozvola);
+        if(data.dozvola!= null){
+
+            user = await taxiModel.findTaxiDriver(data.dozvola);
+        }else if(user.dozvola != null){
+            user = await taxiModel.findTaxiDriver(data.dozvola);
+        }
         
         let qr = genQRCode(user.broj_telefona, user.taksi_dozvola);
         res.render('start.ejs', {user, qr});
@@ -41,12 +48,18 @@ module.exports.cene = async function(req, res, next){
 
 module.exports.artikli = async function(req, res, next){
     try{
+        const data = req.body;
+        //console.log(data)
+        if(data.indikator!= null && user.bodovi >= data.indikator){
+            //console.log(data.indikator)
+            await taxiModel.updatePoints(user.broj_telefona, user.taksi_dozvola, -data.indikator)
+            user = await taxiModel.findTaxiDriver(data.dozvola);
+        }
+       
+        //console.log(user);
         const artikli = await artikalModel.findArticles();
-        res.render('artikli.ejs', {artikli});
+        res.render('artikli.ejs', {artikli, user});
     }catch(err){
         next(err);
     }
 }
-
-
-
