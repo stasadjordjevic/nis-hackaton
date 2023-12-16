be8de9ac9b4015449d6c133e41c6566ddea9c6f4
Lana Matic <lanamatic000@gmail.com>
2023-12-16 09:28:57 +0100

    dovrsena validacija

--- public/validacija.js
+++ public/validacija.js
@@ -5,9 +5,8 @@ if(form.length !=  0){
     form.addEventListener('submit', function(event){
         const dozvola = document.getElementById('dozvola');
         const td = dozvola.value;
-        console.log(td);
         if(td.length !== 5){
-            console.log(`Dozvola mora da sadrzi tacno 5 cifara`);
+            window.alert(`Dozvola mora da sadrzi tacno 5 cifara`);
             event.preventDefault();
             return;
         }
@@ -25,15 +24,14 @@ if(form.length !=  0){
         
         const broj = document.getElementById('telefon');
         const tel = broj.value;
-        console.log(tel);
         if(tel.length !== 10){
-            console.log(`Broj telefona mora da sadrzi tacno 10 cifara`);
+            window.alert(`Broj telefona mora da sadrzi tacno 10 cifara`);
             event.preventDefault();
             return;
         }
         for (let i = 0; i < tel.length; i++) {
             if (!isNumeric(tel[i])) {
-                console.log(`Svi karakteri moraju biti cifre`);
+                window.alert(`Svi karakteri moraju biti cifre`);
                 event.preventDefault();
                 return;
             }
@@ -41,17 +39,15 @@ if(form.length !=  0){
 
         const datum = document.getElementById('datum_rodjenja');
         const d = datum.value;
-        console.log(d);
 
         const parse = new Date(d);
         const currentDate = new Date();
 
-        if(parsedInputDate >= currentDate){
-            console.log(`Datum rodjenja ne moze biti sadasnji niti buduci`);
+        if(parse >= currentDate){
+            window.alert(`Datum rodjenja ne moze biti sadasnji niti buduci`);
             event.preventDefault();
             return;
         }
-
 
     })
 }
